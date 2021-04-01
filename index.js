56a62eb38d83a86071ff3b81f58b06d22cb394e5
moji1x <68261971+moji1x@users.noreply.github.com>
2021-04-01 22:55:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const user = new SteamUser();
 var fs = require("fs");
 config = require(path.resolve("config.json"));
 let configRaw = fs.readFileSync("./config.json").toString();
-/////////////////////////*steam*///////////////////////////////
 
-/////////////////////////////*Include*/////////////////////
 gamesPlayed = config.game
 
 const logOnOptions = {
@@ -35,22 +33,10 @@ user.on('loggedOn', () => {
 	user.gamesPlayed(config.game);
 });
 
-
-	
-user.on('friendRelationship', (steamid, relationship) => {
-  if (relationship === 2) {
-    user.addFriend(steamid);
-    user.chatMessage(steamid, '/pre Hello there! You were auto added by Idler to being keep in friendlist verify Yourself on Discord');
-	user.chatMessage(steamid, '/pre Join my Discord');
-	user.chatMessage(steamid, 'https://discord.gg/9KCP2mg9');
-	user.chatMessage(steamid, "/pre write in discord: !suggest https://steamcommunity.com/profiles/" + steamid + ' . ');
-  }
-});
-
 user.on('friendMessage', function(steamid, message, steamname) {
 	console.log("message from:" + steamid + ": " + message);
 });
-////////////////////////////*Steam things*///////////////////
+
 
 
 
